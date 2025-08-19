619180bd1ac789fa17cf2b921f37480d85b4fd0e
GustavoLoiola <gusantanaloiola@gmail.com>
2025-08-19 15:42:04 -0300

    estudando condições

---
+++ ex008/ok.js
@@ -0,0 +1,8 @@
+var num = 90
+var num2 = 20
+
+if (num >= num2) {
+    console.log('A primeira variável é maior do que a segunda!')
+} else {
+    console.log('A segunda variável é maior do que a primeira!')
+}
---
+++ ex009/index.js
@@ -0,0 +1,9 @@
+console.log(`Teste de velocidade`)
+var vel = 60
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Você passou do limite! MULTADO!`)
+}
+else {
+    console.log(`Você está na velocidade permitida! Dirija com segurança!`)
+}
