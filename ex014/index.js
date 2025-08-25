49b36ce9d80d271388a43d720eb785ef0c3826fc
GustavoLoiola <gusantanaloiola@gmail.com>
2025-08-25 13:40:45 -0300

    finalizando a função de contagem com passos

--- ex014/index.js
+++ ex014/index.js
@@ -3,23 +3,39 @@ function contador() {
     let txtfim = document.querySelector('input#ultimo-num')
     let txtpasso = document.querySelector('input#passo')
     let resposta = document.querySelector('p#resposta')
-    resposta.innerHTML = `${inicio} --> `
-    
-    if (inicio.length == 0 || fim.length == 0 || passo.length == 0) {
-        alert('Dados inválidos!')
+    /*pegando variáveis do HTML*/
+    if (txtinicio.value.length == 0 || txtfim.value.length == 0 || txtpasso.value.length == 0) {
+        alert('Dados inválidos! Por favor preencha todos os campos!')
     }
+    /*Verificando o preenchimento de todos os campos*/
 
-    let inicio = Number(txtinicio.value)
-    const fim = Number(txtfim.value)
-    const passo = Number(txtpasso.value)
+    else {
+        let inicio = Number(txtinicio.value)
+        const fim = Number(txtfim.value)
+        let passo = Number(txtpasso.value)
+        /*passando os valores para Números*/
+        if (passo <= 0) {
+            alert('Passo inválido! Irei considerar o passo como 1.')
+            passo ++
+        }
+        /*Verificando se passo == 0*/
 
-    while(fim >= inicio) {
-        inicio += passo
-        resposta.innerHTML += `${inicio} --> `
-        if (inicio + passo > fim) {
-            break
+        resposta.innerHTML = `${inicio} 👉 `
+
+        if (fim > inicio) {
+            while(inicio + passo <= fim) {
+                inicio += passo
+                resposta.innerHTML += `${inicio} 👉`
+            }
+        }
+        /*Contagem Progressiva */
+        else {
+            while(inicio - passo >= fim) {
+                inicio -= passo
+                resposta.innerHTML += `${inicio} 👉`
         }
     }
-    resposta.innerHTML += 'FIM'
-    
+    /*Contagem Regressiva*/
+        resposta.innerHTML += '🏁'
+    }
 }
