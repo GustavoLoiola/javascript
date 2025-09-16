f3ad4dc7b1e1bb8574d18f50f9df7169841c5a79
GustavoLoiola <gusantanaloiola@gmail.com>
2025-09-16 12:26:11 -0300

    sequência de Fibonacci na web

--- ex019/index9.js
+++ ex019/index9.js
@@ -1,8 +1,33 @@
-let num = document.querySelector("input#txtnumero")
-+num
-let resposta = document.querySelector("p#resposta")
+let Fibonacci = () => {
+    let n = document.querySelector("input#txtnumero")
+    let resposta = document.querySelector("p#resposta")
+    let num = Number(n.value)
+    /*Pega os elementos do HTML*/
 
 
-let Fibonacci = (num) => {
+    if(num <= 0) {
+        alert('Número inválido!')
+    }
+    else if(num == 1) {
+        resposta.innerHTML = '0'
+    }
+    else if(num == 2) {
+        resposta.innerHTML = '0 > 1'
+    }
+    /*Faz uma validação de números inválidos, sendo 0 ou menor inválido, e 1 ou 2 o caso base */
+
+    else {
+    let sequencia = [0,1]
+
+    for(let pos=1; pos < num - 1; pos++) {
+        sequencia.push(sequencia[pos] + sequencia[pos-1])
+    }
+    /*Usa o laço para adicionar números a sequência de acordo com a soma dos dois números anteriores. */
     
+    resposta.innerHTML = ""
+    /*Limpa o Parágrafo caso o usuário queira ver o resultado várias vezes. */
+
+    resposta.innerHTML = sequencia.slice(0, num).join(" > ")
+    /*Adiciona cada elemento da sequência com o slice e usa o join " > " entre cada um deles. */
+}
 }
