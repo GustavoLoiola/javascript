b4df826313b61df3c9d81fece96499bf41a5219e
GustavoLoiola <gusantanaloiola@gmail.com>
2025-12-07 21:15:27 -0300

    rota delete e rota put da api

--- server.js
+++ server.js
@@ -11,29 +11,72 @@ const app = express()
 app.use(express.json())
 /*informando o express que ele usará o formato json */
 
-const users = []
-/*lista de usuários*/
+
 /*MONGO DB
   USUÁRIO: loiola
   SENHA: 27Novembro21 */
 
-app.post('/users', (req, res) => {
-    users.push(req.body)
-    /*adiciona o usuário cadastrado a minha lista users*/
+app.post('/users', async (req, res) => {
+
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+
+
+        }
+    })
+
 
     res.status(201).json(req.body)
     /*status 201 para indicar o sucesso e o meu json cadastra o que for escrito na parte do body dele*/
 
-    res.send('funcionou!')
-    /*apenas para ver se está tudo nos conformes.*/
 })
 
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
+
+    const users = await prisma.user.findMany()
+    
     res.status(200).json(users)
 })
 /*  1) indicar o tipo de rota / método HTTP
     2) indicar o endereço da rota
 */
+
+app.put('/users/:id', async (req, res) => {
+
+       console.log(req)
+
+    await prisma.user.update({
+     
+        where: {
+            id: req.params.id
+        },
+
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+
+
+        }
+    })
+
+
+    res.status(201).json(req.body)
+    /*status 201 para indicar o sucesso e o meu json cadastra o que for escrito na parte do body dele*/
+   
+})
+
+app.delete('/users/:id', async (req,res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.status(200).json({message: "user deleted"})
+})
 
 app.listen(4000)
 /*indica em qual porta a nossa api funcionará*/
