30df2579a2d8d710ec89c324cf023af2a20b0fda
GustavoLoiola <gusantanaloiola@gmail.com>
2026-02-04 17:19:21 -0300

    testando funcionalidades dom

--- ex025/dom1/index1.js
+++ ex025/dom1/index1.js
@@ -1,3 +1,5 @@
+//Função para mudar tema
+
 const btn_light = document.getElementById('lang')
 const CurrentTheme = document.documentElement;
 
@@ -14,3 +16,49 @@ changeTheme = () => {
 }
 
 btn_light.addEventListener('click', changeTheme)
+
+
+
+
+
+
+//Função carrosesel de cards
+
+
+const cardsSection = document.querySelector('#cards');
+const card = document.querySelectorAll('.card');
+const btnLeft = document.querySelector('.arrow.left');
+const btnRight = document.querySelector('.arrow.right');
+
+
+const totalCards = card.length;
+const visibleCards = 5;
+
+
+const cardWidth = card[0].getBoundingClientRect().width + 12; // gap
+
+
+let currentIndex = 0;
+const maxIndex = totalCards - visibleCards;
+
+
+function updateCarousel() {
+  const offset = currentIndex * cardWidth;
+  cardsSection.style.transform = `translateX(-${offset}px)`;
+}
+
+// direita
+btnRight.addEventListener('click', () => {
+  if (currentIndex < maxIndex) {
+    currentIndex++;
+    updateCarousel();
+  }
+});
+
+// esquerda
+btnLeft.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    updateCarousel();
+  }
+});
