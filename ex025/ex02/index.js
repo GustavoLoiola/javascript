67b5f2b24c32c8058ba60808879f8f03899fe96c
GustavoLoiola <gusantanaloiola@gmail.com>
2026-02-10 16:46:04 -0300

    exercício JSON

---
+++ ex025/ex02/index.js
@@ -0,0 +1,42 @@
+// Teste com JSON
+
+/*
+const perus = {
+    cidade: 'São Paulo',
+    habitantes: 90000,
+    zona: 'norte',
+    area: ['recanto', 'russo', 'adelfiore']
+}
+
+let texto = JSON.stringify(perus)
+
+document.getElementById('area').innerHTML = texto
+
+let obj = JSON.parse(texto)
+
+console.log(obj.area[1]) */
+
+const btn = document.getElementById('btn')
+const answer = document.getElementById('text')
+
+function getCEP(){
+   let userCEP = document.getElementById('cep').value
+    
+    const ajax = new XMLHttpRequest()
+    ajax.open('GET', 'https://viacep.com.br/ws/' + userCEP + '/json/')
+    ajax.send()
+
+    ajax.onload = function () {
+
+    let obj = JSON.parse(this.responseText)
+    let rua = obj.logradouro
+    let bairro = obj.bairro
+    let localidade = obj.localidade
+    let estado = obj.estado
+    let ddd = obj.ddd
+
+     answer.innerHTML = 'rua: ' + rua + '<br> bairro: ' + bairro + '<br> local: ' + localidade + '<br> estado: ' + estado + '<br> ddd: ' + ddd
+}
+}
+
+btn.addEventListener('click', getCEP)
