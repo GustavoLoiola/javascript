5112fc5f82149bc3a8c9186730b42fc6d7a99867
GustavoLoiola <gusantanaloiola@gmail.com>
2026-03-25 17:14:34 -0300

    adicionando status a API

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ resposta.send(tabela2025)
 app.get('/:sigla', (requisicao, resposta) => {
     const siglaInformada = requisicao.params.sigla.toUpperCase()
     const time = tabela2025.find(infoTime => infoTime.sigla === siglaInformada)
-    resposta.send(time)
+    if(!time) {
+        resposta.status(404).send('Não existe na série A do brasileirão nenhum time com a sigla informada.')
+        return
+    }
+    resposta.status(200).send(time)
 })
 
 app.listen(3000, () => console.log('servidor rodando com sucesso!'))
--- tabela.js
+++ tabela.js
@@ -1,4 +1,5 @@
-const tabela2025 = {
+const tabela2025 = [
+    {
     nome: 'palmeiras',
     sigla: 'PAL',
     pontos: 55,
@@ -8,6 +9,19 @@ const tabela2025 = {
     golsMarcados: 42,
     golsSofridos: 21,
     saldoGols: 21
-}
+    },
+
+    {
+    nome: 'corinthians',
+    sigla: 'COR',
+    pontos: 7,
+    vitorias: 1,
+    empates: 4,
+    derrotas: 20,
+    golsMarcados: 8,
+    golsSofridos: 71,
+    saldoGols: -63,
+    }
+]
 
 export default tabela2025
