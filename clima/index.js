603c4718f1d999d40a817f8571ac1a15f9e20711
GustavoLoiola <gusantanaloiola@gmail.com>
2026-04-02 16:16:04 -0300

    adicionando as imagens do projeto

---
+++ clima/index.js
@@ -0,0 +1,7 @@
+const key = '4f49024084d955995e077c00af97457e'
+const input = document.querySelector("input#city")
+const btnClick = () => {
+    console.log(input.value)
+}
+
+input.addEventListener('click', btnClick)
