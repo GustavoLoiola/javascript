825ad0ebfd79620d6b79b2c6ff7d67320dd44297
GustavoLoiola <gusantanaloiola@gmail.com>
2026-04-03 22:16:30 -0300

    fazendo o script do projeto

--- clima/index.js
+++ clima/index.js
@@ -1,7 +1,28 @@
 const key = '4f49024084d955995e077c00af97457e'
-const input = document.querySelector("input#city")
-const btnClick = () => {
-    console.log(input.value)
+const city = document.querySelector("input#city")
+const btn = document.querySelector("button#btn")
+
+
+const dataView = (data) => {
+    document.querySelector("div#name").innerHTML = `Tempo em ${data.name}`
+    document.querySelector("div#number").innerHTML = `${Math.floor(data.main.temp)}°C`
+    document.querySelector("p#desc").innerHTML = data.weather[0].description
+    document.querySelector("div#humidity").innerHTML = `Umidade: ${data.main.humidity.toFixed(0)}%`
+    document.querySelector("img#weather-icon").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    document.querySelector("img#weather-icon").style.visibility = 'visible'
+
+    console.log(data)
 }
 
-input.addEventListener('click', btnClick)
+ const searchCity = async (city) => {
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then(response => response.json())
+
+    dataView(data)
+}
+
+const btnClick = () => {
+    searchCity(city.value)
+}
+
+
+btn.addEventListener('click', btnClick)
